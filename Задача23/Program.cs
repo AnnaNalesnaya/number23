25af71b556f51dfa7c9749b9344784a888181175
Anna Nalesnaya <nalesnaya.anna@mail.ru>
2022-09-12 13:06:46 +0300

    не выводит решение консоль

---
+++ Задача23/Program.cs
@@ -0,0 +1,14 @@
+﻿// Напишите программу, кот принимает число (N) и выдает таблицу кубовчисел от 1 до N
+Console.WriteLine("Введите число ");
+int num = Convert.ToInt32(Console.ReadLine());
+GetCubNumber(num);
+
+void GetCubNumber(int num)
+{
+    int count = 1;
+    while(count <= num);
+    {
+        Console.WriteLine($"квадрат числа {count} = {count*count*count}");
+        count++;
+    }
+}   
--- Задача23/obj/Debug/net6.0/Задача23.AssemblyInfo.cs
+++ Задача23/obj/Debug/net6.0/Задача23.AssemblyInfo.cs
@@ -10,12 +10,12 @@
 using System;
 using System.Reflection;
 
-[assembly: System.Reflection.AssemblyCompanyAttribute("ДЗ Семинар 3")]
+[assembly: System.Reflection.AssemblyCompanyAttribute("Задача23")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
 [assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
-[assembly: System.Reflection.AssemblyProductAttribute("ДЗ Семинар 3")]
-[assembly: System.Reflection.AssemblyTitleAttribute("ДЗ Семинар 3")]
+[assembly: System.Reflection.AssemblyProductAttribute("Задача23")]
+[assembly: System.Reflection.AssemblyTitleAttribute("Задача23")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
 
 // Создано классом WriteCodeFragment MSBuild.
