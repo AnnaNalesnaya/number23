6381d9cd0283daae9d8e5a4fe0c0ac2bb0b93397
Anna Nalesnaya <nalesnaya.anna@mail.ru>
2022-09-13 19:34:56 +0300

    Задача 23

---
+++ Задача23/задача 23,1/Program.cs
@@ -0,0 +1,13 @@
+﻿// See https://aka.ms/new-console-template for more information
+Console.WriteLine("введите число ");
+int number = Convert.ToInt32(Console.ReadLine());
+void GetCubNumber(int num)
+{
+    int count = 1;
+    while (count <= Math.Abs(num))
+    {
+        Console.WriteLine ($"куб числa {count} is {Math.Pow(count, 3)}");
+        count++;
+    }
+}
+GetCubNumber(number);
